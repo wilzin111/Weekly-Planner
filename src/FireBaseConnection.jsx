938be9604c3260229b7cdcc7ca3b77abeb3ae9aa
Willian Massalai Tolfo <willianmassalai@gmail.com>
2023-06-14 12:12:03 -0300

    adição fireBaseConfig e inicio do signup

---
+++ src/FireBaseConnection.jsx
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCXXUoST-koLdStXu-F4g8eiWq9U2usDkY",
+    authDomain: "weekly-planner-4f511.firebaseapp.com",
+    projectId: "weekly-planner-4f511",
+    storageBucket: "weekly-planner-4f511.appspot.com",
+    messagingSenderId: "738921286631",
+    appId: "1:738921286631:web:3324be5fcb995121384b42",
+    measurementId: "G-10150SQN85"
+}
+
+const firebaseApp = initializeApp(firebaseConfig)
+
+const db = getFirestore(firebaseApp)
+
+export { db }
